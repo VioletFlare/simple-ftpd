c18cbe74ec0589ee3be9988c19abb1a9227e4d3a
Sven Mawby <fr34kyn01535@bam.yt>
2019-01-21 18:34:34 +0100

    Allowing to set the external host ip with opts.externalHost

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const FTPSession = require('./lib/session')
 function ftpd (opts, sessionCb) {
   if (typeof opts === 'string') {
     const [ host, port ] = opts.split(':')
-    opts = { host: host, port: parseInt(port, 10) }
+    opts = { host: host, externalHost:  host,port: parseInt(port, 10) }
   }
 
   opts = Object.assign({
     host: '127.0.0.1',
+    externalHost: '127.0.0.1',
     port: 1337,
     root: '/',
     maxConnections: 10,
@@ -24,7 +25,7 @@ function ftpd (opts, sessionCb) {
   if (sessionCb) {
     ftpServer.on('connection', (socket) => {
       const session = new FTPSession(socket, {
-        root: opts.root, host: opts.host, readOnly: opts.readOnly
+        root: opts.root, host: opts.host,externalHost:opts.externalHost, readOnly: opts.readOnly
       })
       session.accept(opts.greet).then(() => {
         sessionCb(session)
